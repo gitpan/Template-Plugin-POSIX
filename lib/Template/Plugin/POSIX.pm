92b2b8ef9b269965847d3956d48e8afa1fc06937
Yichun Zhang (章亦春) <agentzh@gmail.com>
2007-03-04 22:33:24 +0000

    Import of AGENT/Template-Plugin-POSIX-0.03 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-POSIX
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AGENT/Template-Plugin-POSIX-0.03.tar.gz
    gitpan-cpan-author:       AGENT
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/POSIX.pm
+++ lib/Template/Plugin/POSIX.pm
@@ -1,18 +1,18 @@
-#: Template/Plugin/POSIX.pm
-#: Implementation for the POSIX Plugin module
-#: Template-Plugin-POSIX v0.02
-#: Copyright (c) 2005 Agent Zhang
-#: 2005-07-17 2005-07-20
-
 package Template::Plugin::POSIX;
 
+use 5.006001;
 use strict;
+use warnings;
+
 use POSIX ();
+use Data::Dumper;
 use Template::Plugin;
 use base qw( Template::Plugin );
-use vars qw( $AUTOLOAD );
+use vars qw( $AUTOLOAD $VERSION );
 
-our $VERSION = '0.01';
+$VERSION = '0.03';
+
+$Data::Dumper::Indent = 0;
 *throw = \&Template::Plugin::POSIX::throw;
 
 sub new {
@@ -22,20 +22,38 @@ sub new {
     }, $class;
 }
 
+my $entered = 0;
+
 sub AUTOLOAD {
     my $self = shift;
     my $method = $AUTOLOAD;
     #warn "$method";
-    
+
     $method =~ s/.*:://;
     return if $method eq 'DESTROY';
 
     #warn "\@_ = @_\n";
-    my $sub = \&{"POSIX::$method"};
-    unless ($sub) {
-        $self->throw("$method not found\n");
+    if ($entered == 1) {
+        die("$method not found\n");
     }
-    return &$sub(@_);
+    my @args;
+    foreach my $arg (@_) {
+        my $code = Data::Dumper->Dump([$arg], ['args']);
+        $code =~ s/^\s*\$args\s*=\s*(.*);\s*$/$1/s;
+        $code =~ s/^\[(.*)\]\s*$/$1/s;
+        push @args, $code;
+    }
+    my $code = "POSIX::$method(".join(',', @args).")";
+    #warn "code: $code\n";
+    $entered = 1;
+    my @retval = eval $code;
+    $entered = 0;
+    if ($@) {
+        $self->throw("POSIX function error: $@");
+    }
+    if (!@retval) { return (); }
+    if (@retval == 1) { $retval[0] }
+    else { \@retval };
 }
 
 sub throw {
@@ -43,17 +61,16 @@ sub throw {
     die (Template::Exception->new('Plugin POSIX', join(', ', @_)));
 }
 
-sub pow {
-    shift;
-    return $_[0] ** $_[1];
-}
-
 1;
 __END__
 
 =head1 NAME
 
-Template::Plugin::POSIX - Plugin to Import POSIX Functions Provided by Perl
+Template::Plugin::POSIX - TT2 plugin to import POSIX built-in functions
+
+=head1 VERSION
+
+This document describes Template::Plugin::POSIX 0.03 released on 5 March, 2007.
 
 =head1 SYNOPSIS
 
@@ -72,38 +89,68 @@ Template::Plugin::POSIX - Plugin to Import POSIX Functions Provided by Perl
 =head1 DESCRIPTION
 
 As a TT programmer, I found it quite inflexible to use the Template Toolkit's
-presentation language Language due to the very limited vocabulary. So I wrote 
+presentation language Language due to the very limited vocabulary. So I wrote
 this little plugin in order to open a window for the template file to the full
-richness of most POSIX functions, making the Template language a 
+richness of most POSIX functions, making the Template language a
 "programming language" in a much more serious sense.
 
-Arguments of all POSIX.* functions are passed by values, and returned in 
-scalar context, so some functions for list data, like B<map> and B<grep>, 
-make little sense in this context.
-
-Please keep in mind I just used AUTOLOAD and Perl's L<POSIX> module to do the
+Please keep in mind I just used AUTOLOAD, eval, and Data::Dumper to do the
 magic here.
 
-If you're looking for smart Perl built-in functions, I suggest you take a 
-look at the L<Template::Plugin::Perl> module which exports the excellent 
-POSIX repertoire.
+If you're looking for even more functions, I suggest you take a look at the
+L<Template::Plugin::Perl> module which exports the excellent POSIX repertoire.
+
+=head1 METHODS
+
+=over
+
+=item new
+
+Constructor called by the TT2 template system
+
+=item throw
+
+TT2 exception handling procedure.
+
+=back
+
+=head1 TODO
+
+=over
+
+=item *
+
+Add more unit tests.
+
+=back
+
+=head1 SOURCE CONTROL
+
+You can always get the latest version of the source code from
+the follow Subversion repository:
+
+L<http://svn.openfoundry.org/ttposix>
+
+There is anonymous access to all.
+
+If you'd like a commit bit, please let me know :)
+
+=head1 AUTHOR
+
+Agent Zhang, E<lt>agentzh@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2005, 2006, 2007 by Agent Zhang. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
 L<Template>,
-L<POSIX>,
-L<Template::Plugin::Perl>
-
-=head1 AUTHOR
-
-Agent Zhang, E<lt>agent2002@126.comE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2005 Agent Zhang.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.1 or,
-at your option, any later version of Perl 5 you may have available.
+L<Template::Plugin::Perl>,
+L<Data::Dumper>
 
 =cut
+
